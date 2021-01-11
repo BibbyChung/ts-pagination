5ddd12457769987e558d3f0764d565070d3fbc52
Bibby <bibbynet@gmail.com>
2021-01-11 10:44:47 +0800

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,269 +1,127 @@
-'use strict';
+const through = require('through2');
+const gulp = require('gulp');
+const spawn = require('child_process').spawn;
 
-let gulp = require('gulp');
-let shell = require('gulp-shell');
-let merge = require('merge-stream');
-let rimraf = require('rimraf');
-let through = require('through2');
+//= ================================== Global Variable ===================================
 
-let ts = require('gulp-typescript');
-let sourcemaps = require('gulp-sourcemaps');
-let path = require('path');
 
-//=================================== Method ===================================
+//= ================================== Method ===================================
 
-let tsCompiler = (
-  pathArr,
-  tsconfigPath,
-  sourceRoot,
-  targetPath,
-  isUglify) => {
+const replaceSpecialWording = (str, replaceWording, id) => {
+  if (str.indexOf(replaceWording) == -1) {
+    return str;
+  }
 
-  return gulp.src(pathArr)
-    .pipe(sourcemaps.init())
-    .pipe(ts.createProject(tsconfigPath)())
-    .js
-    //.pipe(uglify())
-    .pipe(sourcemaps.write('./', {
-      includeContent: false,
-      sourceRoot: sourceRoot,
-    }))
-    .pipe(gulp.dest(targetPath));
+  if (str.indexOf(' ==> ') != -1) {
+    return str;
+  }
+
+  return str.replace(`${replaceWording} `, `${replaceWording} ${id} ==> `);
+}
+
+const getSpecialLetters = (len) => {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
+  let out = '';
+
+  for (let i = 0, clen = chars.length; i < len; i++) {
+    out += chars.substr(0 | Math.random() * clen, 1);
+  }
+
+  return out;
+}
+
+const generateUIDNotMoreThan1million = function () {
+  return ('0000' + (Math.random() * Math.pow(36, 4) << 0).toString(36)).slice(-4)
+}
+
+const getNewBuffer = (buffer) => {
+  const str = buffer.toString();
+  const arr = str.split('\n');
+
+  if (arr.length == 0){
+    return buffer;
+  }
+
+  let id = getSpecialLetters(4);
+  //const id = generateUIDNotMoreThan1million();
+  const newArr = [];
+
+  if (arr[0].indexOf('@') != 0) {
+    newArr.push(`@${id}`);
+  } else {
+    id = arr[0].replace('@', '')
+  }
+
+  for (let item of arr) {
+    let s = item;
+    s = replaceSpecialWording(s, 'Given', id);
+    s = replaceSpecialWording(s, 'When', id);
+    s = replaceSpecialWording(s, 'Then', id);
+    s = replaceSpecialWording(s, 'And', id);
+    s = replaceSpecialWording(s, 'But', id);
+
+    newArr.push(s);
+  }
+
+  const newStr = newArr.join('\n');
+  return Buffer.from(newStr);
+}
+
+const addTagForFeatureFiles = () => {
+  const toProcess = () => {
+    // creating a stream through which each file will pass
+    const stream = through.obj(function (file, enc, done) {
+      if (file.isBuffer()) {
+        file.contents = getNewBuffer(file.contents);
+      }
+
+      // make sure the file goes through the next gulp plugin
+      this.push(file);
+
+      // tell the stream engine that we are done with this file
+      done();
+    });
+
+    return stream;
+  }
+  
+  return gulp.src('./features/**/*.feature')
+    .pipe(toProcess())
+    .pipe(gulp.dest('./features/'));
 };
 
-let tsdCompiler = function (
-  pathArr,
-  tsconfigPath,
-  targetPath
-) {
 
-  let tscP = ts.createProject(tsconfigPath, {
-    'isolatedModules': false,
+const cmd = (str) => async (cb) => {
+  const arr = str.split(' ');
+  const c0 = arr.shift();
+
+  console.log('exec => ', str);
+  await new Promise((resolve, reject) => {
+    const ssp = spawn(c0, arr, { stdio: 'inherit' });
+    ssp.on('close', (code) => {
+      resolve(code);
+    });
+
+    ssp.on('error', function (err) {
+      reject(err);
+    });
   });
 
-  return gulp.src(pathArr)
-    .pipe(tscP())
-    .dts
-    .pipe(gulp.dest(targetPath));
+  cb();
 };
 
-let getCopyFilesPipe = (sourcePatten, targetPath) => {
-  return gulp.src(sourcePatten)
-    .pipe(gulp.dest(targetPath));
-};
 
-let addTagForFeatureFiles = () => {
-  // creating a stream through which each file will pass
-  let stream = through.obj(function (file, enc, done) {
-    if (file.isBuffer()) {
+//= ================================== Tasks ===================================
 
-      let b = getNewBuffer(file.contents);
-      file.contents = b;
+exports.test = cmd('cucumber-js --require features/tests.setup.js --require features/**/*.ts --format node_modules/cucumber-pretty');
 
-    }
+exports.addTagForFeatureFiles = gulp.series(
+  addTagForFeatureFiles,
+  cmd("cucumber-js --require features/tests.setup.js --require features/**/*.ts --format node_modules/cucumber-pretty"),
+);
 
-    // make sure the file goes through the next gulp plugin
-    this.push(file);
-
-    // tell the stream engine that we are done with this file
-    done();
-  });
-
-  let getNewBuffer = (buffer) => {
-    let str = buffer.toString();
-    let arr = str.split('\n');
-
-    if (arr.length == 0)
-      return buffer;
-
-    let id = getSpecialLetters(4);
-    //let id = generateUIDNotMoreThan1million();
-    let newArr = [];
-
-    if (arr[0].indexOf('@') != 0) {
-      newArr.push(`@${id}`);
-    } else {
-      id = arr[0].replace('@', '')
-    }
-
-    for (let item of arr) {
-      let s = item;
-      s = replaceSpecialWording(s, 'Given', id);
-      s = replaceSpecialWording(s, 'When', id);
-      s = replaceSpecialWording(s, 'Then', id);
-      s = replaceSpecialWording(s, 'And', id);
-      s = replaceSpecialWording(s, 'But', id);
-
-      newArr.push(s);
-    }
-
-    let newStr = newArr.join('\n');
-    return new Buffer(newStr);
-  }
-
-  let replaceSpecialWording = (str, replaceWording, id) => {
-    if (str.indexOf(replaceWording) == -1)
-      return str;
-
-    if (str.indexOf(' ==> ') != -1)
-      return str;
-
-    return str.replace(`${replaceWording} `, `${replaceWording} ${id} ==> `);
-  }
-
-  let getSpecialLetters = (len) => {
-    let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_',
-      out = '';
-
-    for (let i = 0, clen = chars.length; i < len; i++) {
-      out += chars.substr(0 | Math.random() * clen, 1);
-    }
-
-    return out;
-  }
-
-  let generateUIDNotMoreThan1million = function () {
-    return ('0000' + (Math.random() * Math.pow(36, 4) << 0).toString(36)).slice(-4)
-  }
-
-  return stream;
-};
-
-//=================================== Tasks ===================================
-
-gulp.task('clean', (cb) => {
-  rimraf('./test', () => {
-    rimraf('./dist', cb);
-  });
-});
-
-gulp.task('addTagForFeatureFiles', () => {
-  return gulp.src('./src/**/*.feature')
-    .pipe(addTagForFeatureFiles())
-    .pipe(gulp.dest('./src/'));
-})
-
-gulp.task('copy_feature_to_test', () => {
-  let m = merge();
-
-  let test = gulp.src([
-    './src/**/*.feature',
-  ]).pipe(gulp.dest('./test/'));
-  m.add(test);
-
-  return m;
-});
-
-gulp.task('ts_compile_test', () => {
-
-  let m = merge();
-
-  let test = tsCompiler(
-    [
-      './src/**/*.ts',
-    ],
-    'tsconfig_test.json',
-    '../src',
-    './test',
-    false
-  );
-  m.add(test);
-
-  return m;
-});
-
-gulp.task('ts_compile_dist', () => {
-  let m = merge();
-
-  let code = tsCompiler(
-    [
-      './src/code/**/*.ts',
-    ],
-    'tsconfig.json',
-    '../src/code',
-    './dist/code',
-    false
-  );
-  m.add(code);
-
-  let main = tsCompiler(
-    [
-      './src/main.ts',
-    ],
-    'tsconfig.json',
-    '../src',
-    './dist/',
-    false
-  );
-  m.add(main);
-
-  return m;
-});
-
-gulp.task('tsd_compile_dist', () => {
-  let m = merge();
-
-  let code = tsdCompiler(
-    [
-      './src/code/**/*.ts',
-    ],
-    'tsconfig.json',
-    './dist/code'
-  );
-  m.add(code);
-
-  let main = tsdCompiler(
-    [
-      './src/main.ts',
-    ],
-    'tsconfig.json',
-    './dist/'
-  );
-  m.add(main);
-
-
-  return m;
-});
-
-gulp.task('run_cucumber', shell.task([
-  'cucumber.js test/**/*.feature --format progress'
-  //'cucumber.js --format pretty'
-]));
-
-gulp.task('create_ts_definitions', shell.task([
-  'tsc --declaration ./src/main.ts'
-  //'cucumber.js --format pretty'
-]));
-
-//----------------------------------------------------------------------
-
-gulp.task('default', gulp.series(
-  'clean',
-  'addTagForFeatureFiles',
-  gulp.parallel(
-    'ts_compile_test',
-    'ts_compile_dist',
-    'tsd_compile_dist',
-    'copy_feature_to_test',
-  ),
-  'run_cucumber',
-));
-
-
-// gulp.task('default', (cb) => {
-//   runSequence(
-//     'clean',
-//     'addTagForFeatureFiles',
-//     [
-//       'ts_compile_test',
-//       'ts_compile_dist',
-//       'tsd_compile_dist',
-//       'copy_feature_to_test',
-//     ],
-//     [
-//       'run_cucumber',
-//     ],
-//     cb
-//   );
-// });
-
+exports.build = gulp.parallel(
+  cmd("cucumber-js --require features/tests.setup.js --require 'features/**/*.ts'"),
+  cmd('tsc -p ./tsconfig.json'),
+  cmd('tsc -p ./tsconfig.esm5.json'),
+);
